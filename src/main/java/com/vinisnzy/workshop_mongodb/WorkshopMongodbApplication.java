e283a50a6a961f1a3d74f5342fe5542231256577
vinisnzy <viniciusmoraes295651@gmail.com>
2025-07-14 22:05:19 -0300

    initial commit

---
+++ src/main/java/com/vinisnzy/workshop_mongodb/WorkshopMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.workshop_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinisnzy/workshop_mongodb/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.workshop_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
