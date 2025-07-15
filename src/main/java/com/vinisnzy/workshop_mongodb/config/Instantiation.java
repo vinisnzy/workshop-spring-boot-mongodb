d184c4cd580e4ccfccad1af91f3ed6e9286d8421
vinisnzy <manoseilacara123@gmail.com>
2025-07-15 10:19:15 -0300

    feat: create post entity with nested user

--- src/main/java/com/vinisnzy/workshop_mongodb/config/Instantiation.java
+++ src/main/java/com/vinisnzy/workshop_mongodb/config/Instantiation.java
@@ -1,27 +1,43 @@
 package com.vinisnzy.workshop_mongodb.config;
 
+import com.vinisnzy.workshop_mongodb.domain.Post;
 import com.vinisnzy.workshop_mongodb.domain.User;
+import com.vinisnzy.workshop_mongodb.repository.PostRepository;
 import com.vinisnzy.workshop_mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.deleteAll();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        postRepository.deleteAll();
+        userRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        repository.saveAll(Arrays.asList(maria, alex, bob));
+        Post post1 = new Post(null, dateFormat.parse("15/07/2025"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", maria);
+        Post post2 = new Post(null, dateFormat.parse("18/07/2025"), "Bom dia", "Acordei feliz hoje!", maria);
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
--- src/main/java/com/vinisnzy/workshop_mongodb/resources/UserResource.java
+++ src/main/java/com/vinisnzy/workshop_mongodb/resources/UserResource.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
